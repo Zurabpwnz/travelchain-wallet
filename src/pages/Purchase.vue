740858fdfdf49f2477bfd740f6176f2fc91262b1
German Mesky <drgmes@gmail.com>
2017-12-27 02:46:31 +0200

    Created modal-window on Purchase page

--- src/pages/Purchase.vue
+++ src/pages/Purchase.vue
@@ -1,26 +1,42 @@
 <template lang="pug">
     q-layout
+        q-card-title
+            q-icon.head-icon(name="vertical_align_bottom")
+            | Purchased Data - check purchased information
+        q-card-main
+            q-data-table(:data="table" :columns="columns")
+                template(slot="col-action" scope="cell")
+                    q-btn(color="blue" @click="openModal = true")
+                        | {{ cell.data }}
 
-            q-card-title
-                q-icon.head-icon(name="vertical_align_bottom")
-                | Purchased Data - check purchased information
-            q-card-main
-                q-data-table(:data="table" :columns="columns")
-                    template(slot="col-action" scope="cell")
-                        q-btn(color="blue")
-                            | {{ cell.data }}
+        q-card-separator
+        q-card-title
+            q-icon.head-icon(name="search")
+            | Data Search - find and buy information with TT
+        q-card-main
+            q-data-table(:data="table" :columns="columns")
+                template(slot="col-action" scope="cell")
+                    q-btn(color="blue")
+                        | {{ cell.data }}
 
-            q-card-separator
-            q-card-title
-                q-icon.head-icon(name="search")
-                | Data Search - find and buy information with TT
-            q-card-main
-                q-data-table(:data="table" :columns="columns")
-                    template(slot="col-action" scope="cell")
-                        q-btn(color="blue")
-                            | {{ cell.data }}
+        q-modal(ref="purchasingData" v-model="openModal" minimized)
+            h4
+                | Purchase Data
 
-            
+            q-input(
+                v-model="purchaseCount"
+                placeholder="0.0"
+                type="number"
+                suffix="TT"
+            )
+
+            br
+
+            q-btn(@click="$refs.purchasingData.close()" color="red")
+                | Decline
+
+            q-btn(@click="$refs.purchasingData.close()" color="green")
+                | Send request data
 </template>
 
 <script lang="ts">
@@ -37,6 +53,7 @@
         QCardActions,
         QCardSeparator,
         QDataTable,
+        QModal,
         QIcon,
         QBtn,
     } from 'quasar';
@@ -52,11 +69,14 @@
             QCardActions,
             QCardSeparator,
             QDataTable,
+            QModal,
             QIcon,
             QBtn,
         }
     })
     export default class Purchase extends Vue {
+        public openModal = false
+        public purchaseCount = 0
 
         public columns = [
             {
