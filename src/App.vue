15b52da84802fa79869e3ac09840ecddbbbf5af8
German Mesky <drgmes@gmail.com>
2018-01-12 22:36:24 +0200

    Improved menu list && Added active label for active menu

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,49 @@
 <template lang="pug">
+    div#q-app
+        q-layout(ref="layout" view="lHh Lpr fff" :left-class="{'bg-grey-2': true}")
+            q-toolbar(slot="header" color="#2c3d75")
+
+                q-btn(v-if="auth.isLoggedIn" flat @click="$refs.layout.toggleLeft()")
+                    q-icon(name="menu")
+
+                img.header-logo(src="/statics/logo.svg")
+
+                q-toolbar-title TravelChain
+                    div(slot="subtitle") Data Wallet
+
   
-  div#q-app 
-    //-Don't drop "q-app" class
-    q-layout(ref="layout" view="lHh Lpr fff" :left-class="{'bg-grey-2': true}")
-      q-toolbar(slot="header" color="#2c3d75")
+            div(v-if="auth.isLoggedIn" slot="left")
+                q-list(no-border link inset-delimiter)
+                    q-item(v-if="auth.isLoggedIn" @click="$router.push('/')")
+                        q-item-side(icon="face")
+                        q-item-main(:label="username")
+                        div
+                            p.no-margin {{ auth.userBalance }} TT
+                    q-list-header Menu
 
-        q-btn(flat @click="$refs.layout.toggleLeft()")
-          q-icon(name="menu")
-
-        img.header-logo(src="/statics/logo.svg")
-      
-        q-toolbar-title TravelChain
-          div(slot="subtitle") Data Wallet
-
-        
-  
-  
-      div(slot="left")
-        q-list(no-border link inset-delimiter)
-          q-item(v-if="auth.isLoggedIn" @click="$router.push('/')")
-            q-item-side(icon="face")
-            q-item-main(:label="username")
-            div
-              p.no-margin
-              | {{ auth.userBalance }} TT
-          q-list-header Menu
-          
-          q-item(v-if="auth.isLoggedIn" @click="$router.push('/')")
-            q-item-side(icon="home")
-            q-item-main(label="Dashboard" sublabel="Manage your account")
-          
-          q-item(v-if="auth.isLoggedIn" @click="$router.push('/data')")
-            q-item-side(icon="data_usage")
-            q-item-main(label="My Data" sublabel="Publish data into the blockchain")
-          
-          q-item(v-if="auth.isLoggedIn" @click="$router.push('/purchase')")
-            q-item-side(icon="assignment_returned")
-            q-item-main(label="Buy data" sublabel="Purchase data from the blockchain")
-
-          q-item(v-if="auth.isLoggedIn" @click="$router.push('/proposals')")
-            q-item-side(icon="monetization_on")
-            q-item-main(label="My proposals" sublabel="Proposals for the purchase of my data")  
-         
-          q-item(v-if="!auth.isLoggedIn" @click="$router.push('/sign-in')")
-            q-item-side(icon="fa-sign-in")
-            q-item-main(label="Login" )
-
-          q-item(v-if="auth.isLoggedIn" @click="logout")
-            q-item-side(icon="exit_to_app")
-            q-item-main(label="Log Out")
+                    template(v-for="link in menuLinks" v-if="!link.isAuth || auth.isLoggedIn")
+                        q-item(@click="link.href ? $router.push(link.href) : link.click()" :class="isCurrentPage(link.href)")
+                            q-item-side(:icon="link.icon")
+                            q-item-main(:label="link.name" :sublabel="link.title")
     
-      div(:class="'page-'+ getPage()")
-        router-view
+            div(:class="'page-'+ getPage()")
+                router-view
 
-    ai-notifier
+        ai-notifier
 </template>
 
 <script lang="ts">
-/*
- * Root component
- */
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import Blockchain from './modules/blockchain.js'
-import store from '../node_modules/store'
-import Dacom from './modules/dacom.js'
-import Notifier from './modules/Notifier'
-import { State, Mutation } from 'vuex-class'
+    import Vue from 'vue';
+    import Component from 'vue-class-component';
+    import Blockchain from './modules/blockchain.js'
+    import store from '../node_modules/store'
+    import Dacom from './modules/dacom.js'
+    import Notifier from './modules/Notifier'
+    import {State, Mutation} from 'vuex-class'
 
 
-import './modules/class-component-hooks';
-import {
-    dom,
-    event,
-    openURL,
-    QLayout,
-    QToolbar,
-    QToolbarTitle,
-    QBtn,
-    QIcon,
-    QList,
-    QListHeader,
-    QItem,
-    QItemSide,
-    QItemMain
-} from 'quasar';
-
-
-@Component({
-    name: 'App',
-    components: {
+    import './modules/class-component-hooks';
+    import {
         QLayout,
         QToolbar,
         QToolbarTitle,
@@ -100,34 +54,92 @@ import {
         QItem,
         QItemSide,
         QItemMain
-    }
-})
-export default class App extends Vue {
-    // TODO VUEX state
-    isAuth = false;
- 
-    @State auth
+    } from 'quasar';
 
-    @Mutation login
 
-    get username () {
-        return this.auth.username || 'Username'
-    }
+    @Component({
+        name: 'App',
+        components: {
+            QLayout,
+            QToolbar,
+            QToolbarTitle,
+            QBtn,
+            QIcon,
+            QList,
+            QListHeader,
+            QItem,
+            QItemSide,
+            QItemMain
+        }
+    })
+    export default class App extends Vue {
 
-    getPage () {
-        return window.location.pathname.substring(1)
-    }
+        @State auth
+        @Mutation login
 
-    mounted () {
-        Blockchain.init()
-        .catch(() => document.write('Not connected to node!'))
-    }
+        private menuLinks = [
+            {
+                href: "/",
+                name: "Dashboard",
+                title: "Manage your account",
+                icon: "home",
+                isAuth: true,
+            },
 
-    logout () {
-        store.remove('account')
-        store.remove('wifs')
-        location.reload()
-        // TODO VUEX logout
+            {
+                href: "/data",
+                name: "My Data",
+                title: "Publish data into the blockchain",
+                icon: "data_usage",
+                isAuth: true,
+            },
+
+            {
+                href: "/purchase",
+                name: "Buy data",
+                title: "Purchase data from the Blockchain",
+                icon: "assignment_returned",
+                isAuth: true,
+            },
+
+            {
+                href: "/proposals",
+                name: "My proposals",
+                title: "Proposals for the purchase of my data",
+                icon: "monetization_on",
+                isAuth: true,
+            },
+
+            {
+                name: "Log Out",
+                icon: "exit_to_app",
+                click: this.logout,
+                isAuth: true,
+            },
+        ]
+
+        mounted() {
+            Blockchain.init()
+                .catch(() => document.write('Not connected to node!'))
+        }
+
+        get username() {
+            return this.auth.username || 'Username'
+        }
+
+        isCurrentPage(href) {
+            return href && window.location.pathname == href ? "active" : ""
+        }
+
+        getPage() {
+            return window.location.pathname.substring(1)
+        }
+
+        logout() {
+            store.remove('account')
+            store.remove('wifs')
+            location.reload()
+            // TODO VUEX logout
+        }
     }
-}
 </script>
